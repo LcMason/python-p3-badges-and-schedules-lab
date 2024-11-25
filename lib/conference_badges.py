b540517bff269261ece1569ea863ca50da7c047b
Lionel Mason <capitalcookhouse@gmail.com>
2024-11-25 16:18:07 -0500

    completes badges/schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
+    
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+   return [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
+   # review this syntax.
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
+
+printer(["Guido", "Edsger"])
+        
